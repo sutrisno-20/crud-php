cc6c5b2e221eed6c836a4ae8bc7cede1f7a97595
Sutrisno <masutrisn021@gmail.com>
2024-03-04 22:17:59 +0700

    feat:create routing

--- index.php
+++ index.php
@@ -4,13 +4,21 @@
         $path = "views/".$url.".php";
         switch ($url) {
             case '':
-                include "views/home.php";
-                break;
-            case "home":
-                include $path;
+                include "views/login.php";
                 break;
             case "register":
                 require_once 'connect.php';
+                $data = ['title'=>'R E G I S T E R'];
+                include $path;
+                break;
+            case "login":
+                require_once 'connect.php';
+                $data = ['title'=>'L O G I N'];
+                include $path;
+                break;
+            case "dashboard":
+                require_once 'connect.php';
+                $data = ['title'=>'D A S H B O A R D'];
                 include $path;
                 break;
             default:
